9d6d017306be31ccaab52ad7849cbbd9394c51bb
brunoalbrito <bruno.brito@clickbus.com>
2020-12-25 21:21:41 -0300

    Initial commit

---
+++ src/main/java/br/bruno/jib/ecstest/EcsTestApplication.java
@@ -0,0 +1,13 @@
+package br.bruno.jib.ecstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/bruno/jib/ecstest/EcsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.bruno.jib.ecstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
